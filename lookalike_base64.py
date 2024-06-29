f87dc316bb1e5171874e4e52b2d1c5018efa5d32
mahdi <147645370+mahdi13377@users.noreply.github.com>
2024-06-29 06:33:55 +0200

    Update lookalike_base64.py

--- lookalike_base64.py
+++ lookalike_base64.py
@@ -1,29 +1,23 @@
 import random
+from collections import defaultdict
 
 sample = "MDEyMzQ1Njc4OWFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVohIiMkJSZcJygpKissLS4vOjs8PT4/QDAxMjM0NTY3ODlhYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ekFCQ0RFRkdISUpLTE1OT1BRUlNUVVZXWFla"
-char_list = list(sample)
 
-char_count = {}
+nodes = [sample[i:i+2] for i in range(0, len(sample)-1, 2)]
 
-for char in char_list:
-    if char in char_count:
-        char_count[char] += 1
-    else:
-        char_count[char] = 1
+node_count = defaultdict(int)
+for node in nodes:
+    node_count[node] += 1
 
-total_chars = len(char_list)
+total_nodes = len(nodes)
+node_probabilities = {node: count / total_nodes for node, count in node_count.items()}
 
-char_percentage = {}
-
-for char, count in char_count.items():
-    char_percentage[char] = (count / total_chars) * 100
-
-characters = list(char_percentage.keys())
-weights = list(char_percentage.values())
-
-weights = [weight / 100 for weight in weights]
+characters = list(node_probabilities.keys())
+weights = list(node_probabilities.values())
 
 def generate(prefix="", length=20):
-    digest = prefix + ''.join(random.choices(characters, weights, k=length))
-    return digest
-
+    selected_nodes = random.choices(characters, weights=weights, k=length)
+    
+    result = prefix + ''.join(selected_nodes)
+    
+    return result
